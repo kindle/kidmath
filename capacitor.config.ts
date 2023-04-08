da9548e3ab5482b17333dbe85d4e47b7c54c7908
bailin wei <weibailin@hotmail.com>
2023-04-08 13:08:34 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'math',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
